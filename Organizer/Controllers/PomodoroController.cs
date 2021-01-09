7963d15d0a60d06f8235018d3f84f4c80cd7c064
Łukasz Kapiński <kapinski.l@gmail.com>
2021-01-09 23:23:29 +0100

    Add project files.

---
+++ Organizer/Controllers/PomodoroController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace Organizer.Controllers
+{
+    [Authorize]
+    public class PomodoroController : Controller
+    {
+        public IActionResult Index(string name)
+        {
+            ViewData["Message"] = "hello " + name;
+            return View();
+        }
+    }
+}
